7e204b108d6e93fbde97866ef3fc5b182e3c5932
massimiliano_arosio_abstract <massimiliano.arosio@abstract.it>
2024-10-18 18:10:24 +0200

    feat(05): imprementation grid fav food without db server

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,3 +1,21 @@
+import Link from 'next/link'
+
+import classes from './page.module.css'
+import MealsGrid from '../components/meals/meals-grid'
+
 export default function Meals() {
-  return <h2>Meals page</h2>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>Delicious meals, created <span className={classes.highlight}>by you </span></h1>
+      </header>
+      <p>Choose your fabourite recipes and cook it yourself. It is easy and fun</p>
+      <p className={classes.cta}>
+        <Link href='/meals/share'>Share your favourite recipe</Link>
+      </p>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  )
 }
