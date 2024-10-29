0495f7db0468f527c9edf9928f3a51ded8e2202c
massimiliano_arosio_abstract <massimiliano.arosio@abstract.it>
2024-10-29 18:06:12 +0100

    feat(12): error handling exemple and not 404 page created

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <main className="not-found">
+            <h1>Not Found</h1>
+            <p>Unfortunatly, we couldnt find the requested page or resources</p>
+        </main>
+    )
+}
