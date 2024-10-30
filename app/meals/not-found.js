27a53d3dfb61dcbe305f3eb5c9a53a486b226c26
massimiliano_arosio_abstract <massimiliano.arosio@abstract.it>
2024-10-30 11:23:20 +0100

    feat(13): load meal page from db and add error page not found slug meal

---
+++ app/meals/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <main className="not-found">
+            <h1>Meal not Found</h1>
+            <p>Unfortunatly, we couldnt find the requested page or meal data</p>
+        </main>
+    )
+}
